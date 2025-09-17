f60dbb81026a349afe70e5ba03120a2bfe743d78
Jander da Costa Nery <jander.webmaster@gmail.com>
2025-09-17 11:14:52 -0400

    Feat: config use .env

--- backend-qrcode-menu/src/infra/modules/app.module.ts
+++ backend-qrcode-menu/src/infra/modules/app.module.ts
@@ -1,9 +1,15 @@
 import { AppService } from '@application/services/app.service';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from '@presenters/controllers/app/app.controller';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- backend-qrcode-menu/src/main.ts
+++ backend-qrcode-menu/src/main.ts
@@ -1,8 +1,15 @@
+import { AppModule } from '@infra/modules/app.module';
+import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const config = app.get(ConfigService);
+  const port = config.get<number>('SERVER_PORT') ?? 3000;
+  const logger = new Logger('Main');
+  await app.listen(port, () => {
+    logger.log(`Application is running on: http://localhost:${port}`);
+  });
 }
 bootstrap();
