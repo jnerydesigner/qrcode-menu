7bec69444d00488f62bbf40427591f42f99c305b
Jander da Costa Nery <jander.webmaster@gmail.com>
2025-12-08 18:40:19 -0400

    feat: introduce TabUserApplication component utilizing new Card and Tabs UI primitives.

---
+++ frontend-qrcode-menu/src/components/TabUserApplication.tsx
@@ -0,0 +1,105 @@
+import { AppWindowIcon, CodeIcon } from "lucide-react"
+
+import { Button } from "@/components/ui/button"
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import {
+    Tabs,
+    TabsContent,
+    TabsList,
+    TabsTrigger,
+} from "@/components/ui/tabs"
+
+export function TabUserApplication() {
+    return (
+        <div className="flex w-full max-w-sm flex-col gap-6">
+            <Tabs defaultValue="mobile">
+                <TabsList>
+                    <TabsTrigger value="mobile">Celular</TabsTrigger>
+                    <TabsTrigger value="desktop">Computador</TabsTrigger>
+                    <TabsTrigger value="tablet">Tablet</TabsTrigger>
+                </TabsList>
+                <TabsContent value="mobile">
+                    <Card>
+                        <CardHeader>
+                            <CardTitle>Celular</CardTitle>
+                            <CardDescription>
+                                Make changes to your account here. Click save when you&apos;re
+                                done.
+                            </CardDescription>
+                        </CardHeader>
+                        <CardContent className="grid gap-6">
+                            <div className="grid gap-3">
+                                <Label htmlFor="tabs-demo-name">Name</Label>
+                                <Input id="tabs-demo-name" defaultValue="Pedro Duarte" />
+                            </div>
+                            <div className="grid gap-3">
+                                <Label htmlFor="tabs-demo-username">Username</Label>
+                                <Input id="tabs-demo-username" defaultValue="@peduarte" />
+                            </div>
+                        </CardContent>
+                        <CardFooter>
+                            <Button>Save changes</Button>
+                        </CardFooter>
+                    </Card>
+                </TabsContent>
+                <TabsContent value="desktop">
+                    <Card>
+                        <CardHeader>
+                            <CardTitle>Computador</CardTitle>
+                            <CardDescription>
+                                Change your password here. After saving, you&apos;ll be logged
+                                out.
+                            </CardDescription>
+                        </CardHeader>
+                        <CardContent className="grid gap-6">
+                            <div className="grid gap-3">
+                                <Label htmlFor="tabs-demo-current">Current password</Label>
+                                <Input id="tabs-demo-current" type="password" />
+                            </div>
+                            <div className="grid gap-3">
+                                <Label htmlFor="tabs-demo-new">New password</Label>
+                                <Input id="tabs-demo-new" type="password" />
+                            </div>
+                        </CardContent>
+                        <CardFooter>
+                            <Button>Save password</Button>
+                        </CardFooter>
+                    </Card>
+                </TabsContent>
+                <TabsContent value="tablet">
+                    <Card>
+                        <CardHeader>
+                            <CardTitle>Tablet</CardTitle>
+                            <CardDescription>
+                                Change your password here. After saving, you&apos;ll be logged
+                                out.
+                            </CardDescription>
+                        </CardHeader>
+                        <CardContent className="grid gap-6">
+                            <div className="grid gap-3">
+                                <Label htmlFor="tabs-demo-current">Current password</Label>
+                                <Input id="tabs-demo-current" type="password" />
+                            </div>
+                            <div className="grid gap-3">
+                                <Label htmlFor="tabs-demo-new">New password</Label>
+                                <Input id="tabs-demo-new" type="password" />
+                            </div>
+                        </CardContent>
+                        <CardFooter>
+                            <Button>Save password</Button>
+                        </CardFooter>
+                    </Card>
+                </TabsContent>
+            </Tabs>
+        </div>
+    )
+}
--- frontend-qrcode-menu/src/components/sections/FinalResultSection.tsx
+++ frontend-qrcode-menu/src/components/sections/FinalResultSection.tsx
@@ -1,10 +1,12 @@
+import { TabUserApplication } from "../TabUserApplication";
+
 export default function FinalResultSection() {
     return (
         <section className="py-24 bg-gray-50 px-6 text-center">
             <h2 className="text-3xl font-semibold">Resultado Final</h2>
 
             <div className="mt-12 flex justify-center">
-                <div className="w-80 h-96 bg-gray-300 rounded-xl"></div>
+                <TabUserApplication />
             </div>
         </section>
     );
